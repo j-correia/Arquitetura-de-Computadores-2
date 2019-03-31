7deeba1577e8b49a6e2efdde1eb52cb0815647c6
João Correia <mr.joao.correia@gmail.com>
2019-04-01 00:14:45 +0100

    Exercício Preparação

--- TesteP1/tp_g1p1e1.s
+++ TesteP1/tp_g1p1e1.s
@@ -3,4 +3,12 @@
 # 	printStr("AC2 - DETPIC32\n");
 #	return 0;
 # }
-
+	.equ PRINT_STR, 8
+msg:	.asciiz "AC2 - DETPIC32\n"
+	.text
+	.globl main
+main:	la $a0, msg			# $a0 = address[msg]
+	ori $v0, $0, PRINT_STR		# $a1 = 8
+	syscall				# printStr(msg)
+	ori $v0, $0, 0			# return 0
+	jr $ra				#
