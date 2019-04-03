dc7863830d085c916b8389172bae204777e38f70
João Correia <mr.joao.correia@gmail.com>
2019-04-03 18:30:53 +0100

    Exercícios Preparação

---
+++ TesteP1/tp_g4p1e5.c
@@ -0,0 +1,36 @@
+#import <detpic32.h>
+void delay(int ms);
+int main(void)
+{
+	unsigned char segment;
+	int bit;
+	int i;
+	LATD = (LATD & 0xFF9F) | 0x0040;
+	LATB = LATB & 0x80FF;
+	TRISB = TRISB & 0x80FF;		// RB8..14 as output
+	TRISD = TRISD & 0xFF9F;		// RD5..6 as output
+	while(1)
+	{
+		LATDbits.LATD6 = !LATDbits.LATD6;
+		LATDbits.LATD5 = !LATDbits.LATD5;
+		segment = 1;
+		for(i = 0; i < 7; i++)
+		{
+			// send segment to display
+			bit = segment << 8;
+			LATB = (LATB & 0x00FF) + bit;
+			delay(500);
+			segment = segment << 1; 
+		}
+	}
+	return 0;
+}
+
+void delay(int ms)
+{
+	for(; ms > 0; ms--)
+	{
+		resetCoreTimer();
+		while(readCoreTimer() < 20000);
+	}
+}
